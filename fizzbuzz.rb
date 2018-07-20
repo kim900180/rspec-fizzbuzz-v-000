b7131b7630bf11a54aa16ca456424beb028562bb
Jae Yong Kim <kim900180@gmail.com>
2018-07-20 20:01:29 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-  if "#{number}" % 3 == 0
-    return "Fizz"
-  elsif "#{number}" % 5 == 0
-    return "buzz"
-  elsif "#{number}" % 3 == 0 && "#{number}" % 5 == 0
-    return "Fizzbuzz"
+  if "#{number}.to_i" % 3 == 0
+    puts "Fizz"
+  elsif "#{number}.to_i" % 5 == 0
+    puts "buzz"
+  elsif "#{number}.to_i" % 3 == 0 && "#{number}.to_i" % 5 == 0
+    puts "FizzBuzz"
   else
-    return nil
+    puts nil
 end
